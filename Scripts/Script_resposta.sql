c4d1c56bd56ce5adbd5c06c86ee70532ee14828b
Caio França <caioanalyst@gmail.com>
2023-02-13 01:50:44 -0300

    Atualizei o script de respostas

--- Scripts/Script_resposta.sql
+++ Scripts/Script_resposta.sql
@@ -1,7 +1,7 @@
 # 1- Quais embarcações possuem pontuação de risco igual a 310?
 
-SELECT NOME_NAVIO, MES_ANO, CLASSIFICACAO_RISCO, INDICE_CONFORMIDADE, PONTUACAO_RISCO, TEMPORADA FROM CAP02.TB_NAVIOS
-WHERE PONTUACAO_RISCO = 310;
+selct nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
+where pontuacao_risco = 310;
 
 
 # 2- Quais embarcações têm classificação de risco A e índice de conformidade maior ou igual a 95%?
@@ -15,6 +15,11 @@ where classificacao_risco = 'A' and indice_conformidade >= 95;
 select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
 where classificacao_risco in ('C','D') and indice_conformidade <= 95;
 
+OU
+
+select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
+where classificacao_risco ='C' or classificacao_risco = 'D' and indice_conformidade <= 95;
+
 
 #4- Quais embarcações têm classificação de risco A ou pontuação de risco igual a 0?
 
@@ -26,3 +31,8 @@ where classificacao_risco = 'A' and pontuacao_risco = 0;
 
 select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
 where mes_ano = '12/2016';
+
+OU
+
+select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
+where temporada like '%Dezembro 2016';
