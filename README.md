391e78773eaac15f564b9a09b43b51fc85dec7aa
Caio França <caioanalyst@gmail.com>
2023-02-17 15:30:09 -0300

    Readm e Script
    Put the read me and script SQL in english.

--- README.md
+++ README.md
@@ -1,5 +1,23 @@
 # Projeto_Navios_SQL
 
+Inspections by the National Health Surveillance Program for Cruise Ships are carried out by Anvisa inspectors and aim to assess the health safety of food and water for human consumption offered on board. The Cleaning and Disinfection Plan (PLD) of the cabins is also monitored, the water in the swimming pools and whirlpools is checked, the management of solid waste, the air conditioning system and the onboard hospital, where it is possible to analyze the number of visits, as well as the health conditions of passengers who underwent consultations on site.
+
+This project aimed to carry out an analysis of data in inspections made by ANVISA. Such a project was developed in the 'SQL for Data Science' course at the Data Science Academy.
+
+The data has been loaded into MySQL and the following questions have been answered:
+
+1- Which vessels have a risk score equal to 310?
+
+2- Which vessels have risk classification A and compliance rate greater than or equal to 95%?
+
+3- Which vessels have risk classification C or D and compliance rate less than or equal to 95%?
+
+4- Which vessels have risk classification A or risk score equal to 0?
+
+5- Which vessels were inspected in December 2016?
+
+--------------------------------------------------------------------------------------------------
+
 As inspeções pelo Programa Nacional de Vigilância em Saúde para Navios de Cruzeiros são realizadas pelos fiscais da Anvisa e tem o objetivod de avaliar a segurança sanitária dos alimentos e da água para consumo humano ofertados a bordo. É realizado também o acompanhamento do Plano de Limpeza e Desinfecção (PLD) das cabines, verificação das águas das piscinas e hidromassagens, o gerenciamento de resíduos sólidos, o sistema de climatização e o hospital de bordo, onde é possível analisar a quantidade de atendimentos, assim como as condições de saúde dos passageiros que passaram por consultas no local.
 
 Este projeto teve como objetivo realizar uma análise dos dados em inspeções feitas pela ANVISA. Tal projeto foi desenvolvido no curso de 'SQL para Data Science' da Data Science Academy.
--- Scripts/Script_resposta.sql
+++ Scripts/Script_resposta.sql
@@ -1,38 +1,44 @@
+
+# 1- Which vessels have a risk score equal to 310?
 # 1- Quais embarcações possuem pontuação de risco igual a 310?
 
 selct nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
 where pontuacao_risco = 310;
 
 
+# 2- Which vessels have risk classification A and compliance rate greater than or equal to 95%?
 # 2- Quais embarcações têm classificação de risco A e índice de conformidade maior ou igual a 95%?
 
 select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
 where classificacao_risco = 'A' and indice_conformidade >= 95;
 
 
-#3- Quais embarcações têm classificação de risco C ou D e índice de conformidade menor ou igual a 95%?
+# 3- Which vessels have risk classification C or D and compliance rate less than or equal to 95%?
+# 3- Quais embarcações têm classificação de risco C ou D e índice de conformidade menor ou igual a 95%?
 
 select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
 where classificacao_risco in ('C','D') and indice_conformidade <= 95;
 
-OU
+#OR
 
 select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
 where classificacao_risco ='C' or classificacao_risco = 'D' and indice_conformidade <= 95;
 
 
-#4- Quais embarcações têm classificação de risco A ou pontuação de risco igual a 0?
+# 4- Which vessels have risk classification A or risk score equal to 0?
+# 4- Quais embarcações têm classificação de risco A ou pontuação de risco igual a 0?
 
 select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
 where classificacao_risco = 'A' and pontuacao_risco = 0;
 
 
-#5- [DESAFIO] Quais embarcações foram inspecionadas em Dezembro de 2016?
+# 5- Which vessels were inspected in December 2016?
+# 5- Quais embarcações foram inspecionadas em Dezembro de 2016?
 
 select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
 where mes_ano = '12/2016';
 
-OU
+#OR
 
 select nome_navio, mes_ano, classificacao_risco, indice_conformidade, pontuacao_risco, temporada from cap02.TB_NAVIOS
 where temporada like '%Dezembro 2016';
